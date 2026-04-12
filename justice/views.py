9116785881270d4f09e7015c7343e8136f606e52
milan <milansapkota16@gmail.com>
2026-04-12 21:55:07 +0930

    Add class-based views for Youth Justice app
    - YoungPersonListView with prefetch_related optimisation
    - YoungPersonDetailView with related data
    - YoungPersonCreateView for adding new persons
    - OffenceCreateView for recording offences
    - InterventionCreateView and UpdateView
    - CaseWorkerDashboardView filtering high risk persons
    - LoginRequiredMixin applied to all views

--- justice/views.py
+++ justice/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView
+)
+from django.urls import reverse_lazy
+from .models import (
+    YoungPerson, Offence, Intervention,
+    CaseWorker, CourtHearing
+)
 
-# Create your views here.
+
+class YoungPersonListView(LoginRequiredMixin, ListView):
+    model = YoungPerson
+    template_name = 'justice/youngperson_list.html'
+    context_object_name = 'youngpeople'
+
+    def get_queryset(self):
+        return YoungPerson.objects.select_related().prefetch_related(
+            'caseworkers', 'offences', 'interventions'
+        )
+
+
+class YoungPersonDetailView(LoginRequiredMixin, DetailView):
+    model = YoungPerson
+    template_name = 'justice/youngperson_detail.html'
+    context_object_name = 'youngperson'
+
+    def get_queryset(self):
+        return YoungPerson.objects.prefetch_related(
+            'offences', 'interventions', 'hearings'
+        )
+
+
+class YoungPersonCreateView(LoginRequiredMixin, CreateView):
+    model = YoungPerson
+    template_name = 'justice/youngperson_form.html'
+    fields = [
+        'first_name', 'last_name', 'date_of_birth',
+        'gender', 'postcode', 'risk_level'
+    ]
+    success_url = reverse_lazy('justice:youngperson-list')
+
+
+class OffenceCreateView(LoginRequiredMixin, CreateView):
+    model = Offence
+    template_name = 'justice/offence_form.html'
+    fields = [
+        'young_person', 'offence_type', 'date_of_offence',
+        'location', 'severity', 'description'
+    ]
+    success_url = reverse_lazy('justice:youngperson-list')
+
+
+class InterventionCreateView(LoginRequiredMixin, CreateView):
+    model = Intervention
+    template_name = 'justice/intervention_form.html'
+    fields = [
+        'young_person', 'assigned_worker', 'intervention_type',
+        'start_date', 'end_date', 'status', 'outcome'
+    ]
+    success_url = reverse_lazy('justice:youngperson-list')
+
+
+class InterventionUpdateView(LoginRequiredMixin, UpdateView):
+    model = Intervention
+    template_name = 'justice/intervention_form.html'
+    fields = ['status', 'end_date', 'outcome']
+    success_url = reverse_lazy('justice:youngperson-list')
+
+
+class CaseWorkerDashboardView(LoginRequiredMixin, ListView):
+    model = YoungPerson
+    template_name = 'justice/dashboard.html'
+    context_object_name = 'youngpeople'
+
+    def get_queryset(self):
+        return YoungPerson.objects.prefetch_related(
+            'caseworkers', 'offences'
+        ).filter(risk_level='high')
