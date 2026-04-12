6a531c7a906c18747444ecbe33594787ff8c66ce
milan <milansapkota16@gmail.com>
2026-04-12 21:45:55 +0930

    Add URL routing for justice app
    - Home page routes to YoungPersonListView
    - CRUD URLs for YoungPerson
    - Create URLs for Offence and Intervention
    - Dashboard URL for CaseWorker

---
+++ justice/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'justice'
+
+urlpatterns = [
+    path('', views.YoungPersonListView.as_view(), name='home'),
+    path('youngperson/', views.YoungPersonListView.as_view(), name='youngperson-list'),
+    path('youngperson/<int:pk>/', views.YoungPersonDetailView.as_view(), name='youngperson-detail'),
+    path('youngperson/create/', views.YoungPersonCreateView.as_view(), name='youngperson-create'),
+    path('offence/create/', views.OffenceCreateView.as_view(), name='offence-create'),
+    path('intervention/create/', views.InterventionCreateView.as_view(), name='intervention-create'),
+    path('intervention/<int:pk>/update/', views.InterventionUpdateView.as_view(), name='intervention-update'),
+    path('dashboard/', views.CaseWorkerDashboardView.as_view(), name='dashboard'),
+]
